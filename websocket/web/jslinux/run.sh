63a33b3dc3bc2c73e2a1dfe0b5d416080ffe4278
haohao <haoninglinux@gmail.com>
2015-12-06 22:12:01 +0800

    Create run.sh

---
+++ websocket/web/jslinux/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#get linux-2.6.20.tar from http://www.linux-mips.org/pub/linux/mips/kernel/v2.6/
+#/usr/libexec/qemu-kvm  -kernel linux-2.6.20.jslinux/arch/i386/boot/bzImage -hda hda.bin -append "root=/dev/hda  console=ttyS0" -serial stdio -vnc 0.0.0.0:3
+#/usr/libexec/qemu-kvm  -kernel linux-2.6.20.test/arch/i386/boot/bzImage -hda hda.bin -append "root=/dev/hda  console=ttyS0" -serial stdio -vnc 0.0.0.0:3
+#/usr/libexec/qemu-kvm -S  -hda linux-0.2.img   -vnc 0.0.0.0:3
+#ntpdate time.nist.gov
+#http://wiki.qemu.org/Testing
+#  
